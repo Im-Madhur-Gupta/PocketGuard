f9b3ce573271b35c5efcd7942c54856d90ebe399
Madhur_Gupta <madleads2001@gmail.com>
2021-12-12 12:32:17 +0530

    adds broken sign up sign in functionality

--- Web_Frontend/src/components/Login/Login.js
+++ Web_Frontend/src/components/Login/Login.js
@@ -1,18 +1,68 @@
-import React from 'react';
-import './Login.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
-function Login() {
+const saveData = (event) => {
+  sessionStorage.setItem("email", event.target.email.value);
+};
+
+class Login extends React.Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    saveData(event);
+    const req = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
+    fetch("http://localhost:8000/login", {
+      mode: "no-cors",
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    }).then((res) => {
+      this.props.setIsLoggedIn(true);
+      window.location.href="/";
+    });
+    console.log("Logged In");
+  };
+  render() {
     return (
-        <div className="login">
-            <form>
-                <label>E-Mail</label>
-                <input type="email" name='email'></input>
-                <label>Password</label>
-                <input type="password" name='password'></input>
-                <button type="submit">Log In</button>
-            </form>
+      <div className="signin-container">
+        <div className="signin-wrapper">
+          <form className="signin" onSubmit={this.handleSubmit}>
+            <span className="signin-title">Sign In</span>
+            <div className="signin-input-wrapper">
+              <input
+                className="signin-input"
+                type="text"
+                name="email"
+                placeholder="Email"
+              ></input>
+            </div>
+            <div className="signin-input-wrapper">
+              <input
+                className="signin-input"
+                type="password"
+                name="password"
+                placeholder="Password"
+              ></input>
+            </div>
+            <div className="signin-btn-wrapper">
+              <button className="signin-btn">Login</button>
+            </div>
+            <div className="signin-link">
+              <span>
+                Don't have an account? <Link to="/signup"> Register here</Link>
+              </span>
+            </div>
+          </form>
         </div>
+      </div>
     );
+  }
 }
 
 export default Login;
