c2341ca01fa8feef5f3d669cee238447a76597da
foram-patel-0201 <foramjpatel0201@gmail.com>
2021-12-12 18:21:52 +0530

    Add Android

---
+++ App/app/src/main/java/com/tatvaalife/emams/SetUpAccount.java
@@ -0,0 +1,14 @@
+package com.tatvaalife.emams;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SetUpAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up_account);
+    }
+}
