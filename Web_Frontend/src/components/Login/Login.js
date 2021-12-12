70a0516a4610bad5adee0c61c843328677feca93
Madhur_Gupta <madleads2001@gmail.com>
2021-12-12 18:08:15 +0530

    adds styled sign in page

--- Web_Frontend/src/components/Login/Login.js
+++ Web_Frontend/src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import signupimg from "../../images/signup.png";
 import "./Login.css";
 
 const saveData = (event) => {
@@ -24,50 +25,57 @@ const Login = (props) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(req),
-    }).then((res) => {
-      console.log(res);
+    })
+      .then((res) => {
+        console.log(res);
         props.setIsLoggedIn(true);
         // window.location.href="/";
         navigate("/");
-        
-    }).catch((err) => {
-      throw err;
-    });
+      })
+      .catch((err) => {
+        throw err;
+      });
     console.log("Logged In");
   };
-    return (
-      <div className="signin-container">
-        <div className="signin-wrapper">
-          <form className="signin" onSubmit={handleSubmit}>
-            <span className="signin-title">Sign In</span>
-            <div className="signin-input-wrapper">
-              <input
-                className="signin-input"
-                type="text"
-                name="email"
-                placeholder="Email"
-              ></input>
-            </div>
-            <div className="signin-input-wrapper">
-              <input
-                className="signin-input"
-                type="password"
-                name="password"
-                placeholder="Password"
-              ></input>
-            </div>
-            <div className="signin-btn-wrapper">
-              <button className="signin-btn">Login</button>
-            </div>
-            <div className="signin-link">
-              <span>
-                Don't have an account? <Link to="/signup"> Register here</Link>
-              </span>
-            </div>
-          </form>
-        </div>
+  return (
+    <div className="signup-container">
+      <div className="signup-img">
+        <p className="floatingpara">
+          Sign Up to enjoy all of our functionalities.
+        </p>
+        <img src={signupimg} alt="" />
       </div>
-    );
-}
+      <div className="signup-wrapper">
+        <form className="signin" onSubmit={handleSubmit}>
+          <span className="signin-title">Sign In</span>
+          <div className="signin-input-wrapper">
+            <input
+              className="signin-input"
+              type="text"
+              name="email"
+              placeholder="Email"
+            ></input>
+          </div>
+          <div className="signin-input-wrapper">
+            <input
+              className="signin-input"
+              type="password"
+              name="password"
+              placeholder="Password"
+            ></input>
+          </div>
+          <div className="signin-btn-wrapper">
+            <button className="signin-btn">Login</button>
+          </div>
+          <div className="signin-link">
+            <span>
+              Don't have an account? <Link to="/signup"> Register here</Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
