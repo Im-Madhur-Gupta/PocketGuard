31e257ea6123f1e47f101e0dd1f85740cff09879
Madhur_Gupta <madleads2001@gmail.com>
2021-12-12 17:53:31 +0530

    adds styled sign up page

--- Web_Frontend/src/components/Signup/Signup.js
+++ Web_Frontend/src/components/Signup/Signup.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import signupimg from "../../images/signup.png";
 import "./Signup.css";
 
-const Signup = ({setIsSignedUp, isSignedUp}) => {
+const Signup = ({ setIsSignedUp, isSignedUp }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const req = {
@@ -18,63 +19,71 @@ const Signup = ({setIsSignedUp, isSignedUp}) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(req),
-    }).then((res) => {
-      setIsSignedUp(true);
-      window.location.href="/login";
-    }).catch(error => {
-      throw(error);
-    });
+    })
+      .then((res) => {
+        setIsSignedUp(true);
+        window.location.href = "/login";
+      })
+      .catch((error) => {
+        throw error;
+      });
     console.log("SignedUp");
   };
-    return (
-      <div className="signin-container">
-        <div className="signin-wrapper">
-          <form className="signin" onSubmit={handleSubmit}>
-            <span className="signin-title">Sign Up</span>
-            <div className="signin-input-wrapper">
-              <input
-                className="signin-input"
-                type="text"
-                name="firstname"
-                placeholder="First Name"
-              ></input>
-            </div>
-            <div className="signin-input-wrapper">
-              <input
-                className="signin-input"
-                type="text"
-                name="lastname"
-                placeholder="Last Name"
-              ></input>
-            </div>
-            <div className="signin-input-wrapper">
-              <input
-                className="signin-input"
-                type="text"
-                name="email"
-                placeholder="Email"
-              ></input>
-                </div>
-        <div className="signin-input-wrapper">
-              <input
-                className="signin-input"
-                type="password"
-                name="password"
-                placeholder="Password"
-              ></input>
-            </div>
-            <div className="signin-btn-wrapper">
-              <button className="signin-btn">Sign Up</button>
-            </div>
-            <div className="signin-link">
-              <span>
-                Already have an account? <Link to="/login">Sign In</Link>
-              </span>
-            </div>
-          </form>
-        </div>
+  return (
+    <div className="signup-container">
+      <div className="signup-img">
+        <p className="floatingpara">
+          Sign Up to enjoy all of our functionalities.
+        </p>
+        <img src={signupimg} alt="" />
       </div>
-    );
-}
+      <div className="signup-wrapper">
+        <form className="signin" onSubmit={handleSubmit}>
+          <span className="signin-title">Create an Account</span>
+          <div className="signin-input-wrapper">
+            <input
+              className="signin-input"
+              type="text"
+              name="firstname"
+              placeholder="First Name"
+            ></input>
+          </div>
+          <div className="signin-input-wrapper">
+            <input
+              className="signin-input"
+              type="text"
+              name="lastname"
+              placeholder="Last Name"
+            ></input>
+          </div>
+          <div className="signin-input-wrapper">
+            <input
+              className="signin-input"
+              type="text"
+              name="email"
+              placeholder="Email"
+            ></input>
+          </div>
+          <div className="signin-input-wrapper">
+            <input
+              className="signin-input"
+              type="password"
+              name="password"
+              placeholder="Password"
+            ></input>
+          </div>
+          <div className="signin-btn-wrapper">
+            <button className="signin-btn">Create an Account</button>
+          </div>
+          <div className="signin-link">
+            <span>
+              Already have an account? <Link to="/login">Sign In</Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
